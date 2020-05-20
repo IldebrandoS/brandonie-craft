d36bf42f4ed163c821591c9d94539bd77d8462a2
Ildebrando Sagerdahl <sagerdai@ecs.csus.edu>
2020-05-20 12:20:01 -0700

    lights hope src

---
+++ shared/revision.h
@@ -0,0 +1,5 @@
+#ifndef __REVISION_DATA_H__
+#define __REVISION_DATA_H__
+ #define REVISION_HASH                      "1e9c08673b452ce8d53a"
+ #define REVISION_DATE                      "2019-12-01 15:49:58 +0200"
+#endif // __REVISION_DATA_H__
